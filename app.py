ec927945d75a46f3f7bf7f7f515f92ae36fdb211
Alvaro Sena <alvarosenacs.c@gmail.com>
2022-03-18 18:35:13 -0300

    New blueprint: views and feed component.

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, session
 from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
 from models import *
 from auth import auth
+from views import views
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///questions.db'
 db = SQLAlchemy(app)
 
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+app.register_blueprint(views, url_prefix='/')
 app.register_blueprint(auth, url_prefix='/')
 
 
--- auth.py
+++ auth.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template, request, redirect
+from hashlib import new
+from flask import Blueprint, render_template, request, redirect, flash, session, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 import models
 import app
 
-auth = Blueprint('views', __name__)
+auth = Blueprint('auth', __name__)
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -12,15 +13,28 @@ def sign_up():
         email = request.form.get('email')
         password = request.form.get('password')
 
-        # user = User.query.filter_by(email=email).first()
+        user = models.User.query.filter_by(email=email).first()
 
-        # if user:
-        #     pass
-        
-        new_user = models.User(username=username, email=email, password=generate_password_hash(
-                password, method='sha256'))
-        app.db.session.add(new_user)
-        app.db.session.commit()
-        return redirect('/sign-up')
+        if user:
+            flash('User already exists', category='error')
+        elif not username:
+            flash('Username must is incorrect.')
+        elif not email:
+            flash('Email must is incorrect.')
+        elif not password:
+            flash('Password must is incorrect.')
+        elif len(password) < 8:
+            flash('Your password must have 8 chars minimum.', category='error')
+        else:
+            new_user = models.User(username=username, email=email, password=generate_password_hash(
+            password, method='sha256'))
+            app.db.session.add(new_user)
+            app.db.session.commit()
+            session['user_id'] = new_user.id
+            # return redirect(url_for('index'))
 
     return render_template('sign-up.html')
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
